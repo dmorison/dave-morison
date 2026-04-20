d1c623000b1ce2ad87cbbf598a31bc92ef41c09c
David Morison <davemorison@gmail.com>
2026-04-20 17:21:46 +0100

    adding software logo icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,23 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 import partytown from '@astrojs/partytown';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://dmorison.github.io',
     base: '/dave-morison',
+    image: {
+        responsiveStyles: true
+    },
     integrations: [
         partytown({
             config: {
               forward: ["dataLayer.push"],
             },
+        }),
+        icon({
+            iconDir: "src/assets/icons",
         }),
     ],
     experimental: {
